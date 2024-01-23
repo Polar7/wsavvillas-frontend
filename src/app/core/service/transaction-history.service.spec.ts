5587e2f4954e217659374ab19fe2d8c38293b63a
Jean Lozano <polar_rhel@localhost.localdomain>
2024-01-23 18:35:44 -0500

    Test Jasmine

--- src/app/core/service/transaction-history.service.spec.ts
+++ src/app/core/service/transaction-history.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TransactionHistoryService } from './transaction-history.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('TransactionHistoryService', () => {
   let service: TransactionHistoryService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule]});
     service = TestBed.inject(TransactionHistoryService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it("#getTransactionsHistory should get transaction history", (done: DoneFn) => {
+    // Debe ser diferente de nulo
+    service.getTransactionsHistory().subscribe(res => {
+      expect(res).not.toBeNull();
+      done();
+    })
   });
 });
